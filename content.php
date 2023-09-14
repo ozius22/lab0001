ab12c58c1e30043ea514fa196593912646950b6a
Luis <lsuizo72@gmail.com>
2023-09-14 13:51:22 +0800

    first commit

---
+++ content.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>The Cooking Website</title>
+    <link rel="stylesheet" href="./dist/output.css">
+</head>
+<body>
+
+<body class="justify-center flex flex-col">
+    <img class="mt-4" src="flatten.png" alt="">
+</body>
+    
+</body>
+</html>
+
